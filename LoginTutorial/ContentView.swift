1c84d52776f6864fac505ca6012a241d83c09e07
karinamsilva <karinamartinssilva@live.com>
2021-05-21 16:02:22 -0700

    Initial Commit

---
+++ LoginTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginTutorial
+//
+//  Created by user196036 on 5/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginTutorial/LoginTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTutorialApp.swift
+//  LoginTutorial
+//
+//  Created by user196036 on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
